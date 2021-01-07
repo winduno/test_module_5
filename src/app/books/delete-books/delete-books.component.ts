a91cde13d7f9c14abd301a40e8268a3a097bc1d3
winduno <windsolo294@gmail.com>
2021-01-07 16:47:54 +0700

    complete

--- src/app/books/delete-books/delete-books.component.ts
+++ src/app/books/delete-books/delete-books.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BookServiceService} from '../../book-service.service';
+import {ActivatedRoute} from '@angular/router';
+import {Book} from '../../model/book';
 
 @Component({
   selector: 'app-delete-books',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteBooksComponent implements OnInit {
 
-  constructor() { }
+  book: Book = {};
+  id: number;
+  constructor(private bookService: BookServiceService,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe(async paramMap => {
+      // @ts-ignore
+      this.id = +paramMap.get('id');
+      // this.product = await this.getProductById(this.id);
+      this.getBookById(this.id);
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  getBookById(id) {
+    this.bookService.getBookById(id).subscribe(result => {
+      this.book = result;
+    }, error => console.log(error));
+  }
+
+  deleteBook(id) {
+    this.bookService.deleteBook(id).subscribe(() => {
+      console.log('Thành công!!');
+    }, () => {
+      console.log('Lỗi cụ nó rồi');
+    });
+  }
 }
